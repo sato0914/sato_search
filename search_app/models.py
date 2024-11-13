4eb766bc7f19c3e0f61645f23ff6a1d28065f5d9
ribeka <ribeka0914@gmail.com>
2024-11-13 16:16:12 +0900

    1113a

--- search_app/models.py
+++ search_app/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     description = models.TextField(verbose_name=_("Description"))
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Price"))
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name=_("Category"))
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name=_("Product Image"))
 
     def __str__(self):
